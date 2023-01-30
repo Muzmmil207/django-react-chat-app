88492f6eb6daa3da65702df1d4e56d3fba6cfeb1
mozmmil-ali <mly88207@gmail.com>
2023-01-30 08:49:42 +0200

    create user register view

--- backend/apps/chat/api/urls.py
+++ backend/apps/chat/api/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path("", views.get_routes, name="get_routes"),
-    path('users/', views.UserView, name="users_api_view"),
-    path('users-messages/<int:user_id>/', views.UserMessageView, name="message_api_view"),
-
-    path('token/', views.MyTokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
-
+    path("users/", views.UserView),
+    path("register/", views.UserRegisterView),
+    path("users-messages/<int:user_id>/", views.UserMessageView, name="message_api_view"),
+    # tokens views
+    path("token/", views.MyTokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
 ]
--- backend/apps/chat/api/views.py
+++ backend/apps/chat/api/views.py
@@ -1,4 +1,5 @@
 from apps.chat.models import Conversation, User
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from rest_framework import filters, generics, mixins, viewsets
 from rest_framework.decorators import api_view, permission_classes
@@ -18,22 +19,42 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     #     return token
     pass
 
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-    
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_routes(request):
     routes = [
-        '/users',
-        '/users-messages/<int>',
-        '/api/token',
-        '/api/token/refresh',
+        "/api/users",
+        "/api/users-messages/<int>",
+        "/api/register/",
+        "/api/token",
+        "/api/token/refresh",
     ]
     return Response(routes)
 
 
-@api_view(['GET'])
+@api_view(["POST"])
+def UserRegisterView(request):
+    data = request.data
+    user = UserCreationForm(data)
+
+    if User.objects.filter(username=data.get("username")).exists():
+        return Response(
+            {"response": "User with this name already exists, please try another username"}
+        )
+    elif data.get("password1") == None or data.get("password1") != data.get("password2"):
+        return Response({"response": "password did not match"})
+    elif user.is_valid():
+        user.save()
+        return Response({"response": "Done"})
+    else:
+        return Response({"response": "The data dose not valid"})
+
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def UserView(request):
     users = User.objects.exclude(id=request.user.id)
@@ -41,26 +62,27 @@ def UserView(request):
     return Response(serializers.data)
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 def UserMessageView(request, user_id):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.data
-        from_user = User.objects.get(id=data['from_user'])
-        to_user = User.objects.get(id=data['to_user'])
+        from_user = User.objects.get(id=data["from_user"])
+        to_user = User.objects.get(id=data["to_user"])
         Conversation.objects.create(
-            from_user = from_user,
-            to_user = to_user,
-            content = data['content'],
+            from_user=from_user,
+            to_user=to_user,
+            content=data["content"],
         )
 
     users_message = Conversation.objects.filter(
-        Q(from_user=request.user.id, to_user=user_id) |
-        Q(from_user=user_id, to_user=request.user.id)
-        )
+        Q(from_user=request.user.id, to_user=user_id)
+        | Q(from_user=user_id, to_user=request.user.id)
+    )
 
     serializers = ConversationSerializer(users_message, many=True)
     return Response(serializers.data)
+
 
 # class UserView(generics.ListAPIView):
 #     """
@@ -68,4 +90,3 @@ def UserMessageView(request, user_id):
 #     """
 #     queryset = User.objects.all()
 #     serializer_class= UserSerializer
-
